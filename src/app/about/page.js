aaac0ba4f4da5260688be94adafd905710ab3b17
ido barnea <barbareshet@gmail.com>
2023-04-17 20:25:07 +0300

    pages

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,10 +3,10 @@ import { Inter } from 'next/font/google'
 import Link from "next/link";
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function About() {
   return (
     <div>
-      <h1 className="text-5xl text-cyan-700">Home Page</h1>
+      <h1 className="text-5xl text-cyan-700">About Page</h1>
     </div>
   )
 }
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,10 +3,10 @@ import { Inter } from 'next/font/google'
 import Link from "next/link";
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Contact() {
   return (
     <div>
-      <h1 className="text-5xl text-cyan-700">Home Page</h1>
+      <h1 className="text-5xl text-cyan-700">Contact Page</h1>
     </div>
   )
 }
